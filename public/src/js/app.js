f0d440f1cb4fb4d536105299e60e0b6428f3ec54
matheeshaYapa <ylmatheesha@gmail.com>
2019-10-11 17:37:29 +0530

    promises

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,7 +1,45 @@
+var deferredPrompt;
+
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
         .register('/sw.js')
         .then(function () {
             console.log('Service Worker Registered!');
+        })
+        .catch(function (err) {
+            console.log(err);
         });
 }
+
+window.addEventListener('beforeinstallprompt', function (event) {
+    console.log('beforeinstallprompt fired');
+    event.preventDefault();
+    deferredPrompt = event;
+    return false;
+});
+
+var promise = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        // resolve('After timer is done');
+        reject({code: 500, message: 'An error occurred!'});
+        // console.log('After 3 seconds');
+    }, 3000);
+});
+
+// promise.then(function (text) {
+//     return text;
+// }, function (err) {
+//     console.log(err.code, err.message);
+// }).then(function (newText) {
+//     console.log(newText)
+// });
+
+promise.then(function (text) {
+    return text;
+}).then(function (newText) {
+    console.log(newText)
+}).catch(function (err) {
+    console.log(err.code, err.message)
+});
+
+console.log('Right after setTimeout()');
